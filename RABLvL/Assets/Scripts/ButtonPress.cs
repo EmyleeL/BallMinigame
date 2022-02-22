3437c06246cf217e8e920ffb4827779c96b82256
EmyleeL <EmyLeent@gmail.com>
2022-02-22 11:24:27 -0500

    RABLvL

---
+++ RABLvL/Assets/Scripts/ButtonPress.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour
+{
+
+    // place button items here
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // if button pressed command
+        // change button color (material) for pressed vs unpressed
+        
+    }
+}
---
+++ RABLvL/Assets/Scripts/SimonSays.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimonSays : MonoBehaviour
+{
+    // use buttons in here?
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // create simon says (red, red blue, red blue green, red blue green yellow, etc.)
+        // randomize simon says pattern
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // did press correct button? continue pattern
+        // didn't press correct button? restart.
+        // correct after 5? game win
+        // game win? open door to "next level"
+    }
+}
